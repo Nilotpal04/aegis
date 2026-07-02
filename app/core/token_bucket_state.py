7590efefcc83c1597e5dda4252178d625eef0595
Nilotpal04 <nilotpalb2004@gmail.com>
2026-07-02 12:52:14 +0000

    add unit tests for token bucket algorithm

--- app/core/token_bucket_state.py
+++ app/core/token_bucket_state.py
@@ -3,6 +3,5 @@ from uvicorn.protocols.http import flow_control
 
 @dataclass
 class TokenBucketState:
-    token: float
+    tokens: float
     last_refill: float
-    
---
+++ tests/test_token_bucket.py
@@ -0,0 +1,63 @@
+import time
+from app.storage.memory import InMemoryStorage
+from app.algorithms.token_bucket import TokenBucket
+from app.core.token_bucket_state import TokenBucketState
+
+def test_first_request_is_allowed():
+    storage = InMemoryStorage[TokenBucketState]()
+    limiter = TokenBucket(
+        capacity=3,
+        refill_rate=1,
+        storage=storage,
+    )
+    
+    result = limiter.allow("user123")
+    
+    assert result is True
+    state = storage.get("user123")
+    assert state is not None
+    assert state.tokens == 2
+    
+def test_bucket_capacity_is_enforced():
+    storage = InMemoryStorage[TokenBucketState]()
+    limiter = TokenBucket(
+        capacity=3,
+        refill_rate=1,
+        storage=storage,
+    )
+    
+    assert limiter.allow("user123") is True
+    assert limiter.allow("user123") is True
+    assert limiter.allow("user123") is True
+    assert limiter.allow("user123") is False
+    
+def test_tokens_refill_after_time():
+    storage = InMemoryStorage[TokenBucketState]()
+    limiter = TokenBucket(
+        capacity=2,
+        refill_rate=1,
+        storage=storage,
+    )
+    
+    assert limiter.allow("user123") is True
+    assert limiter.allow("user123") is True
+    assert limiter.allow("user123") is False
+    
+    time.sleep(1.1)
+    assert limiter.allow("user123") is True
+    
+def test_users_are_rate_limited_independently():
+    storage = InMemoryStorage[TokenBucketState]()
+    limiter = TokenBucket(
+        capacity=2,
+        refill_rate=1,
+        storage=storage,
+    )
+    
+    assert limiter.allow("Neel") is True
+    assert limiter.allow("Neel") is True
+    assert limiter.allow("Neel") is False
+
+    assert limiter.allow("Luna") is True
+    assert limiter.allow("Luna") is True
+    assert limiter.allow("Luna") is False
